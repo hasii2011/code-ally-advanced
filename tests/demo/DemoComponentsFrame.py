63a3d5cea08127a47f05904d903d6043a837a781
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-02-08 19:37:08 -0700

    <Initial Setup>[Project]: <Demo Scaffold does 2 components 🍻>
    [I like it]
    
    []

--- tests/demo/DemoComponentsFrame.py
+++ tests/demo/DemoComponentsFrame.py
@@ -6,6 +6,12 @@ from wx import Window
 from wx.lib.sized_controls import SizedPanel
 
 from wx.lib.sized_controls import SizedScrolledPanel
+from wx.lib.sized_controls import SizedStaticBox
+
+from hasiicommon.Dimensions import Dimensions
+from hasiicommon.Position import Position
+from hasiicommon.ui.widgets.DimensionsControl import DimensionsControl
+from hasiicommon.ui.widgets.PositionControl import PositionControl
 
 
 DEFAULT_WIDTH = 3000
@@ -31,4 +37,48 @@ class DemoComponentsFrame(SizedScrolledPanel):
         initPosY:  int = 0
         self.SetScrollbars(PIXELS_PER_UNIT_X, PIXELS_PER_UNIT_Y, nbrUnitsX, nbrUnitsY, initPosX, initPosY, False)
 
+        self._valuesChanged: bool = False
+
         self.SetSizerType('vertical')
+        # noinspection PyUnresolvedReferences
+        self.SetSizerProps(expand=False)
+
+        self._layoutDemoWidgets(sizedScrolledPanel=self)
+
+        self.Fit()
+        self.SetMinSize(self.GetSize())
+
+    def _layoutDemoWidgets(self, sizedScrolledPanel: SizedScrolledPanel):
+
+        labelledPanel: SizedStaticBox = SizedStaticBox(sizedScrolledPanel, label='Widgets')
+        labelledPanel.SetSizerProps(expand=True, proportion=1)
+
+        horizontalPanel: SizedPanel = SizedPanel(labelledPanel)
+        horizontalPanel.SetSizerType('horizontal')
+        horizontalPanel.SetSizerProps(expand=True, proportion=1)
+
+        positionControl: PositionControl = PositionControl(sizedPanel=horizontalPanel, displayText='Demo Position',
+                                                           minValue=0, maxValue=2048,
+                                                           valueChangedCallback=self._positionChanged,
+                                                           setControlsSize=False)
+
+        dimensionsControls: DimensionsControl = DimensionsControl(sizedPanel=horizontalPanel, displayText='Demo Dimensions',
+                                                                  minValue=480, maxValue=4096,
+                                                                  valueChangedCallback=self._dimensionsChanged,
+                                                                  setControlsSize=False)
+
+        # noinspection PyUnresolvedReferences
+        positionControl.SetSizerProps(expand=True, proportion=1)
+        # noinspection PyUnresolvedReferences
+        dimensionsControls.SetSizerProps(expand=True, proportion=1)
+
+        positionControl.position = Position(0, 2048)
+        dimensionsControls.dimensions   = Dimensions(480, 2048)
+
+    def _positionChanged(self, newPosition: Position):
+        self.logger.info(f'Position changed: {newPosition=}')
+        self._valuesChanged = True
+
+    def _dimensionsChanged(self, newDimensions: Dimensions):
+        self.logger.info(f'Dimensions changed: {newDimensions=}')
+        self._valuesChanged = True
